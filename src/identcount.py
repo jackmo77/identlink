97c3c50750fb2f9448fae01579a733adfc7492bb
Theodore Turocy <drarbiter@gmail.com>
2017-12-09 15:05:47 +0000

    Added utility to report on ident coverage by year.

---
+++ src/identcount.py
@@ -0,0 +1,19 @@
+import sys
+import glob
+
+import pandas as pd
+
+def main():
+    year = sys.argv[1]
+
+    for fn in glob.glob("leagues/%s/*.csv" % year):
+        df = pd.read_csv(fn, dtype=str)
+        league = df['league.name'].unique()[0]
+
+        print "%4d/%4d %5.1f%% %s %s" % \
+          (len(df[~df['ident'].isnull()]), len(df),
+           100.0 * len(df[~df['ident'].isnull()]) / len(df),
+           year, league)
+    
+if __name__ == '__main__':
+    main()
