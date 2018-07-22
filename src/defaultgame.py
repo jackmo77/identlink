ec8e77e2f556354c4c0f2b6d9b4795bdd8c97bc4
Theodore Turocy <drarbiter@gmail.com>
2018-07-22 19:46:03 +0100

    Updated idents from upstream sources.

--- src/defaultgame.py
+++ src/defaultgame.py
@@ -6,12 +6,19 @@ import sys
 import pandas as pd
 
 def main():
+    replacechars = [" ", ".", "&", "-"]
     for fn in sys.argv[1:]:
         print fn
         df = pd.read_csv(fn, encoding='utf-8', dtype='str')
-        df['ident'] = df['ident'].fillna(df['game.date'].str[-4:] + \
-                                         df['home.name'].str.replace(" ", "").str[:3].str.lower() + \
-                                         df['game.number'].replace({"0": ""}))
+        team0 = df[['away.name', 'home.name']].min(axis=1).str.lower()
+        team1 = df[['away.name', 'home.name']].max(axis=1).str.lower()
+        for char in replacechars:
+            team0 = team0.str.replace(char, "")
+            team1 = team1.str.replace(char, "")
+        
+        ident = (df['game.date'].str[-4:] + "-" +
+                 team0 + "-" + team1 + "-" + df['game.number'])
+        df['ident'] = df['ident'].fillna(ident)
         df.to_csv(fn, encoding='utf-8', index=False)
                                     
 
