f2a263b9f8393db9b8b6d6ba8583d0836201fc4a
Theodore Turocy <drarbiter@gmail.com>
2018-11-11 15:16:22 +0000

    Updated idents for 1909.

---
+++ src/coverage.py
@@ -0,0 +1,26 @@
+import sys
+import glob
+import argparse
+
+import pandas as pd
+
+def main():
+    """Report on coverage of idents for records by year.
+    """
+    parser = argparse.ArgumentParser(description="Report on ident coverage "
+                                                 "by year")
+    parser.add_argument('year', type=int, help="the year to report")
+    args = parser.parse_args()
+
+    for fn in sorted(glob.glob("data/ident/people/%s/*.csv" % args.year)):
+        df = pd.read_csv(fn, dtype=str)
+        league = df['league.name'].unique()[0]
+
+        print "%4d/%4d %5.1f%% %s %s" % \
+          (len(df[~df['ident'].isnull()]), len(df),
+           100.0 * len(df[~df['ident'].isnull()]) / len(df),
+           args.year, league)
+
+
+if __name__ == '__main__':
+    main()
